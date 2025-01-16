f4a17aef7a6576949fced4eeb23ad8d538c11184
Gonzalez-Itzel <itzgonzalez@ucdavis.edu>
2025-01-16 00:36:28 -0800

    Update

---
+++ 10demo.py
@@ -0,0 +1,19 @@
+# 10demo.py by Itzel Gonzalez
+
+import math
+
+print('hello, again') # greeting
+print(1.5e-2)
+print(278 - 198)
+print(14 * 4)
+print(2 ** 10)
+print(12 // 3)
+print(10 % 3)
+print(6 * (4/2))
+print(abs(-9))
+print(pow(3,4))
+print(round(3.12829, ndigits=3))
+print(math.ceil(3.5))
+print(math.log2(24))
+print(math.sqrt(36))
+print(math.pi)
