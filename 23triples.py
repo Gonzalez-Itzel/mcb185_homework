1a5eaf72990249215087945c7c80818629f45f07
Gonzalez-Itzel <itzgonzalez@ucdavis.edu>
2025-02-10 16:38:23 -0800

    Update

--- 23triples.py
+++ 23triples.py
@@ -5,9 +5,9 @@ import math
 limit = 100 # determines the limit of variable 'a' and 'b'
 
 
-for a in range(1, limit):
-	for b in range (1, limit):
+for a in range(1, limit): # side a triangle
+	for b in range (1, limit): # side b of a triangle
 	 	c = math.sqrt(a**2 + b**2)
-	 	remainder = c % 1
-	 	if remainder  == 0: print(a, b, c)
+	 	remainder = c % 1 # we want only integer
+	 	if remainder  == 0: print(a, b, c) # if there is no remainder, it is an integer
 		
