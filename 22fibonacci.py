82dadf386eda62382a6ef11b5d7b58dfaabba37a
Gonzalez-Itzel <itzgonzalez@ucdavis.edu>
2025-02-10 16:31:43 -0800

    Update

---
+++ 22fibonacci.py
@@ -0,0 +1,15 @@
+f1 = 0
+f2 = 1
+print (f1)
+print (f2)
+
+for i in range(0,8):
+		total = f1 + f2 # store the sum in total
+		f1 = f2 # our f2 value in the equation above is our new f1 
+		f2 = total # our total value in the equation above is our new f2 
+		print(total)
+
+
+
+
+	
