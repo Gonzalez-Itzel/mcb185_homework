fbdaefe31e586558fc0de34955fad6c22db3318a
Gonzalez-Itzel <itzgonzalez@ucdavis.edu>
2025-03-02 11:18:34 -0800

    update

--- 35scoringmatrix.py
+++ 35scoringmatrix.py
@@ -5,25 +5,25 @@ seq = sys.argv[1]
 match_score = sys.argv[2]
 mismatch_score = sys.argv[3]
 
-print("   " + "  ".join(seq))
+print('   ' + '  '.join(seq))
 
 for i in range(0, len(seq)):
-    print(seq[i], end=" ")  
+    print(seq[i], end=' ')  
     for j in range(0, len(seq)): 
         if seq[i] == seq[j]: 
-            print(f"{match_score:>2}", end=" ") 
+            print(f"{match_score:>2}", end=' ') 
         else:
-            print(f"{mismatch_score:>2}", end=" ")  
+            print(f"{mismatch_score:>2}", end=' ')  
     print() 
 
 # assessment without using no range, no f strings, and no join
-print("   ", end='')
+print('   ', end='')
 for nt in seq:
-	print(nt, end="  ")
+	print(nt, end='  ')
 print()
 
 for nt1 in seq:
-	print(nt1, end= " ")
+	print(nt1, end= ' ')
 	for nt2 in seq:
 		if nt1 == nt2: print(match_score, end=' ')
 		else: print(mismatch_score, end=' ')
